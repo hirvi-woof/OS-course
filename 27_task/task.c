4864b87d37c4d9c136cab3ac8272586911a15a09
hirvi <45123982+hirvi-woof@users.noreply.github.com>
2020-06-03 01:37:12 +0700

    fixed

--- 27_task/task.c
+++ 27_task/task.c
@@ -1,59 +1,40 @@
-#include <unistd.h>
 #include <stdlib.h>
-#include <wait.h>
-#include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#include <errno.h>
 #define SIZE 256
 
 
 int main(int argc, char* argv[])
 {
-	FILE *file, *channel;
-        char *ptr;
-	int err, size;
-	char buff[SIZE];
-	char empty_line[] = "\n";
-	if (argc < 2) 
-	{
-        	err = printf("error - not enough arguments; usage: %s file_name\n", argv[0]);
-        	exit(-1);
-    	}
-    	file = fopen(argv[1], "r");
-	if(file == 0)
-	{
-		perror("can't call fopen");
-		exit(-1);
-	}
-	channel = popen("wc -l", "w");
-	if(channel == 0)
-	{
-		perror("can't call popen");
-		exit(-1);
-	}
-	do
-	{
-		ptr = fgets(buff, SIZE, file);
-		if(!strcmp(empty_line, buff))
-		{
-			fputs(empty_line, channel);
-		}
-	} while(ptr);
+        FILE *channel;
+        int err, result;
+        char res[SIZE], cmd[SIZE];
+        if (argc < 2)
+        {
+                err = printf("error - not enough arguments; usage: %s file_name\n", argv[0]);
+                exit(-1);
+        }
+        strcat(cmd, "cat ");
+        strcat(cmd, argv[1]);
+        strcat(cmd, " | egrep \"^$\" | wc -l");
 
-	err = fclose(file);
-	if(err == -1)
-	{
-		perror("can't call fclose");
-		exit(-1);
-	}
-	err = pclose(channel);
-	if(err == -1)
-	{
-		perror("can't call pclose");
-		exit(-1);
-	}
-	return 0;
+        channel = popen(cmd, "r");
+
+        if(channel == 0)
+        {
+                perror("can't call popen");
+                exit(-1);
+        }
+
+        fgets(res, SIZE, channel);
+        result = atoi(res);
+        printf("result: %d\n", result);
+
+        err = pclose(channel);
+        if(err == -1)
+        {
+                perror("can't call pclose");
+                exit(-1);
+        }
+        return 0;
 }
-
